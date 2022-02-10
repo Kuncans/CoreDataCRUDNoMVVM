216eeb74aea42286d14e937e75d252df036036c4
Kuncans <akafauna@gmail.com>
2022-02-10 03:53:59 +0000

    Initial Commit

---
+++ CoreDataMA1/CoreDataMA1App.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMA1App.swift
+//  CoreDataMA1
+//
+//  Created by Duncan Kent on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMA1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
